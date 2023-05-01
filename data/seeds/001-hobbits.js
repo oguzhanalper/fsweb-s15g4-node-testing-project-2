f40cf23ebf340542cb4a544a4fc8469f6c3356e9
oguzhanalper06 <alperhanoguz06@gmail.com>
2023-05-01 22:13:21 +0300

    seed oluşturuldu

---
+++ data/seeds/001-hobbits.js
@@ -0,0 +1,14 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.seed = async function (knex) {
+  // Deletes ALL existing entries
+  await knex("hobbits").truncate();
+  await knex("hobbits").insert([
+    { id: 1, name: "sam" },
+    { id: 2, name: "frodo" },
+    { id: 3, name: "pippin" },
+    { id: 3, name: "merry" },
+  ]);
+};
